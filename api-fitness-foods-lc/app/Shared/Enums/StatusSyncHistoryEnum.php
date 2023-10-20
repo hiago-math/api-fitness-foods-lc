2dd7a3c03ad8e947dabf77d166a12e73704c9af2
Hiago Moreira <oliverhia.math@gmail.com>
2023-10-19 22:02:18 -0300

    feat: adicionando enums de status para produto e sync de importacao

--- api-fitness-foods-lc/app/Shared/Enums/StatusSyncHistoryEnum.php
+++ api-fitness-foods-lc/app/Shared/Enums/StatusSyncHistoryEnum.php
@@ -4,5 +4,8 @@ namespace Shared\Enums;
 
 class StatusSyncHistoryEnum
 {
-
+    public const STARTED = 'start';
+    public const PROCESSING = 'processing';
+    public const FINISHED = 'finished';
+    public const ERROR = 'error';
 }
